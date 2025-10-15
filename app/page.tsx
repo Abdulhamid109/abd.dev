5944e496899fc2ab61aee086903970a7543035ea
Abdulhamid Patel <2000147400@hexaware.com>
2025-10-15 17:08:46 +0530

    implemented scrollTrigger using gsap

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client"
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Page1 from "@/components/PersonalComps/page1";
 import Page2 from "@/components/PersonalComps/page2";
 import Page3 from "@/components/PersonalComps/page3";
@@ -7,20 +10,50 @@ import Page5 from "@/components/PersonalComps/page5";
 import Page6 from "@/components/PersonalComps/page6";
 
 export default function Page() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animate each page with ScrollTrigger
+    gsap.utils.toArray(".page").forEach((page, i) => {
+      gsap.from(page as any, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: page as any,
+          start: "top 80%",
+          end: "top 20%",
+          scrub: false,
+          markers: false, // Set to true for debugging
+        },
+      });
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
   return (
-    <div className="">
-      <div>
+    <div className="" ref={containerRef}>
+      <div className="page">
         <Page1/>
       </div>
-      <div>
+      <div className="page">
         <Page2/>
       </div>
-      <div className="">
+      <div className="page">
         <Page3/>
       </div>
-      <div>
+      <div className="page">
         <Page4/>
+      </div>
+      <div className="page">
         <Page5/>
+        
+      </div>
+      <div className="page">
         <Page6/>
       </div>
       
